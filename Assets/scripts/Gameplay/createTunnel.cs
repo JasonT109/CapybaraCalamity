a9fc9e185d66637d0758c20b159155336bdf8ec5
JasonT109 <jason.tassell@gmail.com>
2016-10-28 10:14:22 +1300

    Tunnels refined.

--- Assets/scripts/Gameplay/createTunnel.cs
+++ Assets/scripts/Gameplay/createTunnel.cs
@@ -6,7 +6,8 @@ public class createTunnel : MonoBehaviour {
     public bool canGrow = false;
     public float growthRate = 0.1f;
     public float digspeed = 1.0f;
-    public GameObject wall;
+    public GameObject Wall;
+    public GameObject Scaler;
 
 	// Use this for initialization
 	void Start () {
@@ -15,16 +16,16 @@ public class createTunnel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        while (transform.localScale.x < 1 && canGrow)
+        while (Scaler.transform.localScale.x < 1 && canGrow)
         {
             StartCoroutine(WaitAndGrow(growthRate));
             canGrow = false;
         }
-        if (transform.localScale.x >= 1 && canGrow)
+        if (Scaler.transform.localScale.x >= 1 && canGrow)
         {
-            if (wall != null)
+            if (Wall != null)
             {
-                Destroy(wall);
+                Destroy(Wall);
             }
             //stop the agent from moving
             canGrow = false;
@@ -33,7 +34,7 @@ public class createTunnel : MonoBehaviour {
     IEnumerator WaitAndGrow(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        transform.localScale += new Vector3(digspeed, 0, 0);
+        Scaler.transform.localScale += new Vector3(digspeed, 0, 0);
         canGrow = true;
     }
 }
